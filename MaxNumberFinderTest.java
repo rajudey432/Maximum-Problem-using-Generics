4413e90e690a8f1c37a9a949798548b3d038ba75
raju dey <rajudey432@gmail.com>
2023-04-25 19:44:55 +0530

    welcome to find maximum problems using generics

---
+++ MaxNumberFinderTest.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.generics;
+
+public class MaxNumberFinderTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Generics");
+    }
+}
